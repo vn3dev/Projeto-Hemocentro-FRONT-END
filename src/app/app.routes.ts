196b45d86b6d4c7c062386f43e00fdb76bc63285
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-05-21 21:20:51 -0300

    fix: remove visao geral

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { DoadoresList } from './components/doadores-list/doadores-list';
 import { BolsaLista } from './components/bolsa-lista/bolsa-lista';
-import { VisaoGeral } from './components/visao-geral/visao-geral';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'doadores', pathMatch: 'full' },
   { path: 'doadores', component: DoadoresList },
   { path: 'bolsas', component: BolsaLista },
-  { path: 'visao-geral', component: VisaoGeral },
 ];
